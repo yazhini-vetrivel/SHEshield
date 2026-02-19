c8968d8e560a0f4165ce252a8dcaf023502899c1
yazhini-vetrivel <cb.sc.u4cse24760@cb.students.amrita.edu>
2026-02-19 11:40:27 +0530

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,3 +1,5 @@
-VONAGE_API_KEY=your_nexmo_api_key
-VONAGE_API_SECRET=your_nexmo_api_secret
-VONAGE_FROM_NUMBER=SafetyApp  # You can use a name or a Nexmo number
+import os
+
+VONAGE_API_KEY = os.getenv('VONAGE_API_KEY')
+VONAGE_API_SECRET = os.getenv('VONAGE_API_SECRET')
+VONAGE_FROM_NUMBER = os.getenv('VONAGE_FROM_NUMBER')
