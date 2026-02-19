ec033c0caa7131a4ed39bec0d7bb75a57a4d7a76
yazhini-vetrivel <cb.sc.u4cse24760@cb.students.amrita.edu>
2026-02-19 16:08:06 +0530

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,7 +1,9 @@
+from django.contrib import admin
 from django.urls import path
-from .views import TriggerSOS, HeatmapDataView
+from safety_app.views import TriggerSOS, HeatmapData
 
 urlpatterns = [
-    path('api/sos/', TriggerSOS.as_view(), name='trigger-sos'),
-    path('api/heatmap/', HeatmapDataView.as_view(), name='heatmap-data'),
+    path('admin/', admin.site.urls),
+    path('api/sos/', TriggerSOS.as_view()),
+    path('api/heatmap/', HeatmapData.as_view()),
 ]
