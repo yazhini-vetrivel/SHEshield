81788ba674c0d06122b3a353f4b3074d32df213a
yazhini-vetrivel <cb.sc.u4cse24760@cb.students.amrita.edu>
2026-02-19 20:37:42 +0530

    Delete urls.py

--- urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import TriggerSOS, HeatmapDataView
-
-urlpatterns = [
-    path('api/sos/', TriggerSOS.as_view(), name='trigger-sos'),
-    path('api/heatmap/', HeatmapDataView.as_view(), name='heatmap-data'),
-]
